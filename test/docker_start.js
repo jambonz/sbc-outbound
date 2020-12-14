3d2178897c656bc2c1638b14cdb47d2b8a1e46ba
Dave Horton <daveh@beachdognet.com>
2020-12-14 13:36:29 -0500

    ci: healthcheck for mysqldb

--- test/docker_start.js
+++ test/docker_start.js
@@ -4,12 +4,8 @@ const exec = require('child_process').exec ;
 
 test('starting docker network..', (t) => {
   exec(`docker-compose -f ${__dirname}/docker-compose-testbed.yaml up -d`, (err, stdout, stderr) => {
-
-    // wait 5 secs for mysql to be ready for connections
-    setTimeout(() => {
-      t.pass('docker started');
-      t.end(err);  
-    }, 15000);
+    t.pass('docker started');
+    t.end(err);  
   });
 });
 
