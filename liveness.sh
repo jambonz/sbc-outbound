2c4e2cb7f1f6f1c0be5eb61ed46a97daf096f492
Abel Simon <abelsimon48@gmail.com>
2022-03-24 13:30:44 +0200

    install netcat in dockerfile

--- liveness.sh
+++ liveness.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-TCP_SERVER_PORT = "${DRACHTIO_PORT:-4000}"
-nc -v -z localhost $TCP_PORT
+TCP_SERVER_PORT="${DRACHTIO_PORT:-4000}"
+nc -v -z localhost $TCP_SERVER_PORT
 
 # if last command exited with non zero
 if [ $? != 0 ]
@@ -9,7 +9,7 @@ then
     exit 1
 fi
 
-HTTP_SERVER_PORT = "${HTTP_PORT:-3000}"
+HTTP_SERVER_PORT="${HTTP_PORT:-3000}"
 printf 'GET /system-health HTTP/1.1\r\nHost: localhost\r\n\r\n' | nc -v -z localhost 3000
 
 if [ $? != 0 ]
